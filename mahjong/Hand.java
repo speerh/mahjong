93527b5ff1019e08ac1ccc5967d1aded69dd2c70
Addison Zheng <32391554+AddisonSubtract@users.noreply.github.com>
2024-04-06 12:45:14 -0500

    Hand and Tile

--- mahjong/Hand.java
+++ mahjong/Hand.java
@@ -6,4 +6,25 @@ public class Hand {
 	public ArrayList<Tile> tiles;
 	public ArrayList<Tile> discards;
 	
+	public ArrayList<Tile> getTile(){
+		return tiles;
+	}
+	
+	public void addTile(Tile tile){
+		tiles.add(tile);
+	}
+	
+	public void removeTile(int pos){
+		discards.add(tiles.get(pos));
+		tiles.remove(pos);
+	}
+	
+	public ArrayList<Tile> getDiscards(){
+		return discards;
+	}
+	
+	public void removeDiscards(int pos){
+		tiles.add(discards.get(pos));
+		discards.remove(pos);
+	}
 }
--- mahjong/Tile.java
+++ mahjong/Tile.java
@@ -4,13 +4,45 @@ public class Tile {
 	private String suit;
 	private int number;
 	private Boolean terminal;
-	
-	public Tile(String suit, int num, Boolean terminal){
+
+	public Tile(String suit, int num, Boolean terminal) {
 		this.suit = suit;
 		number = num;
 		this.terminal = terminal;
-		
-		//** WINDS: E S W N (1 2 3 4)
-		//** DRAGONS: R G W (1 2 3)
+
+		// ** WINDS: E S W N (1 2 3 4)
+		// ** DRAGONS: R G W (1 2 3)
 	}
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public Boolean getTerminal() {
+		return terminal;
+	}
+
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public void setTerminal(Boolean terminal) {
+		this.terminal = terminal;
+	}
+
+	public void setTile(String suit, int num, Boolean terminal) {
+		setSuit(suit);
+		setNumber(num);
+		setTerminal(terminal);
+	}
+
+	// public getTile() {
 }
