6eb0bc9030935db6c6ed77a8cd2e322a99d65807
Addison Zheng <32391554+AddisonSubtract@users.noreply.github.com>
2024-04-17 12:40:04 -0500

    Yaku first commit

--- src/mahjong/Center.java
+++ src/mahjong/Center.java
@@ -1,5 +1,6 @@
 package mahjong;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -10,6 +11,7 @@ public class Center {
 	private String[] originalTiles = {"bamboo1", "bamboo2", "bamboo3", "bamboo4", "bamboo5", "bamboo6", "bamboo7", "bamboo8", "bamboo9", "character1", "character2", "character3", "character4", "character5", "character6", "character7", "character8", "character9", "circle1", "circle2", "circle3", "circle4", "circle5", "circle6", "circle7", "circle8", "circle9", "dragon1", "dragon2", "dragon3", "dragon4", "wind1", "wind2", "wind3", "wind4"};
 	private static ArrayList<Tile> centerTiles = new ArrayList<Tile>();
 	static Tile tile = new Tile (null, 0, null);
+	static boolean terminal;
 	
 	public Center() {
 		addToArrayList(originalTiles);
@@ -24,7 +26,13 @@ public class Center {
     {
         for (int i = 0; i < 4; i++) {
         	 for (int j = 0; j < arr.length; j++) {
-        		 tile.setTile(arr[j].substring(arr[j].length()-1), Integer.parseInt(arr[j].substring(arr[j].length())), null);
+        		 if(arr.length % 2 == 1 || arr.length % 2 == 9) {
+        			 terminal = true;
+        		 }
+        		 else {
+        			 terminal = false;
+        		 }
+        		 tile.setTile(arr[j].substring(arr[j].length()-1), Integer.parseInt(arr[j].substring(arr[j].length())), terminal);
         		 centerTiles.add(tile);
         	 }
         }
@@ -37,5 +45,9 @@ public class Center {
 	public Tile getFirstTile() {
 		return centerTiles.get(0);
 		
+	}
+	
+	public void shuffle() {
+		Collections.shuffle(centerTiles);
 	}
 }
--- src/mahjong/Yaku.java
+++ src/mahjong/Yaku.java
@@ -1,7 +1,39 @@
 package mahjong;
 
+import java.util.ArrayList;
+
 public class Yaku {
 	private int points;
 	private int distance;
 	private String name;
+	private boolean win;
+	private String suit;
+	private Integer num;
+	private ArrayList<String> newTiles = new ArrayList<String>();
+
+	public void checkWin(Hand hand) {
+
+		for (Tile tile : hand.getTile()) {
+			suit = tile.getSuit();
+			num = tile.getNumber();
+
+			newTiles.add(suit + num);
+		}
+		// Tanyao - hand does not contain terminals, 1's or 9s
+		String[] inputString = { "bamboo2", "bamboo3", "bamboo4", "bamboo5", "bamboo6", "bamboo7", "bamboo8",
+				"character2", "character3", "character4", "character5", "character6", "character7", "character8",
+				"circle2", "circle3", "circle4", "circle5", "circle6", "circle7", "circle8" };
+
+		for (String item : inputString) {
+			if (!newTiles.contains(item)) {
+				win = false;
+				break;
+			}else {
+				name = "Tanyao";
+				points = 40;
+				win = true;
+				break;
+			}
+		}
+	}
 }
